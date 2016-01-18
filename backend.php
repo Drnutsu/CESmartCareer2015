30b6ea37b09cdabb8460995b61f8f05296d45f4b
Nontawat Numor <nontawat39@gmail.com>
2016-01-18 21:15:21 +0700

    delete

--- backend.php
+++ backend.php
@@ -9,10 +9,6 @@
     	die('Not connected : ' . mysql_error());
 	}
 	$db = mysql_select_db("company", $connection);
-<<<<<<< HEAD
-=======
-
->>>>>>> e247596b0a51b0431e6cf7deb110db8e2597b053
 ?>
 
 <!DOCTYPE html>
