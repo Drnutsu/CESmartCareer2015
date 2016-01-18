e4e79e72209219339089017cf55fb21bd8cd128a
Nontawat Numor <nontawat39@gmail.com>
2016-01-18 20:36:53 +0700

    connect Database

---
+++ backend.php
@@ -0,0 +1,55 @@
+<?php 
+	session_start();
+	if (!isset($_SESSION['username'])) {
+		header('Location: admin.php');
+	}
+
+	require('connectDB.php');
+	if (!$connection) {
+    	die('Not connected : ' . mysql_error());
+	}
+	$db = mysql_select_db("company", $connection);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CE Smart Career - Backend</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<!-- Custom styles for this template -->
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="table-responsive">
+					<table class="table table-hover">
+					  <tr>
+					    <td>Jill</td>
+					    <td>Smith</td> 
+					    <td>50</td>
+					  </tr>
+					  <tr>
+					    <td>Jill</td>
+					    <td>Smith</td> 
+					    <td>50</td>
+					  </tr>
+					  <tr>
+					    <td>Jill</td>
+					    <td>Smith</td> 
+					    <td>50</td>
+					  </tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ connectDB.php
@@ -0,0 +1,3 @@
+<?php
+	$connection = mysql_connect("cesmartcamp.com", "jirasak_career", "CEc@reer2016");
+?>
